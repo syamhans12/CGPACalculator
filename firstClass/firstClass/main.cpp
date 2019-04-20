11c3e3667a84cf8bcad54977db0198a35793428f
Syahmi Hanis Mahamad Chah Pari <hanisss125@gmail.com>
2019-04-20 22:48:30 +0800

    llll

--- firstClass/firstClass/main.cpp
+++ firstClass/firstClass/main.cpp
@@ -25,18 +25,18 @@ int main()
 	cout << "\t School Of Computing" << endl;
 	cout << "\t Faculty Of Engineering" << endl;
 	cout << " " << endl;
-	cout << "Enter students name : ";
+	/*cout << "Enter students name : ";
 	cin.getline(mainName, 50,'\n');
 	cout << endl;
 	cout << "Enter students matric number :";
 	cin.getline(mainMatricnum, 50,'\n');
 	st1.setMatricnumber(mainMatricnum);
 	st1.setName(mainName);
-	cout << endl;
+	cout << endl;*/
 	
 
 	
-	cout << "\t 5 Major Course Offered" << endl;
+	/*cout << "\t 5 Major Course Offered" << endl;
 	cout << "\t ->SCSR ( Network & Security )" << endl;
 	cout << "\t ->SCSV (Graphic and Multimedia)" << endl;
 	cout << "\t ->SCSJ (Software Engineering)" << endl;
@@ -49,7 +49,7 @@ int main()
 	//sb1.setCourse(mainCourse);
 	cout << "Choose your semester (1-8) :";
 	cin>>mainSemester;
-	sb1.setSemester(mainSemester);
+	sb1.setSemester(mainSemester);*/
 	cout << "Number of subject taken : ";
 	cin >> numbersubjtaken;
 	
